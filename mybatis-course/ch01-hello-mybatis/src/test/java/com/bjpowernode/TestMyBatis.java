8a8b95b5836c2bb24ca02e5172527864aed45325
QiHang <553446603@qq.com>
2021-02-01 15:53:36 +0800

    1

--- mybatis-course/ch01-hello-mybatis/src/test/java/com/bjpowernode/TestMyBatis.java
+++ mybatis-course/ch01-hello-mybatis/src/test/java/com/bjpowernode/TestMyBatis.java
@@ -5,13 +5,19 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 
-public class MyApp {
-    public static void main(String[] args) throws IOException {
+public class TestMyBatis {
+    /**
+     * 你还等好傻
+     * @throws IOException
+     */
+    //测试方法
+    @Test
+    public void testInsert() throws IOException {
         //访问mybatis读取student数据
         //1.定义mybatis主配置文件的名称, 从类路径的根开始（target/clasess）
         String config="mybatis.xml";
@@ -22,17 +28,23 @@ public class MyApp {
         //4.创建SqlSessionFactory对象
         SqlSessionFactory factory = builder.build(in);
         //5.获取SqlSession对象，从SqlSessionFactory中获取SqlSession
-        SqlSession sqlSession = factory.openSession();
-        //6.【重要】指定要执行的sql语句的标识。  sql映射文件中的namespace + "." + 标签的id值
+        SqlSession sqlSession = factory.openSession(true); //ture设置为自动提交事务
+        //6.[重要]指定要执行的sql语句的标识。  sql映射文件中的namespace + "." + 标签的id值
         //String sqlId = "com.bjpowernode.dao.StudentDao" + "." + "selectStudents";
-        String sqlId = "com.bjpowernode.dao.StudentDao.selectStudents";
-        //7. 重要】执行sql语句，通过sqlId找到语句
-        List<Student> studentList = sqlSession.selectList(sqlId);
+        String sqlId = "com.bjpowernode.dao.StudentDao.insertStudent";
+        //7.[重要]执行sql语句，通过sqlId找到语句
+        Student student = new Student();
+        student.setId(1007);
+        student.setName("张飞");
+        student.setEmail("zhangfei@163.com");
+        student.setAge(20);
+        int nums = sqlSession.insert(sqlId,student);
+        //mybatis默认不是自动提交事务的，所以在insert，update，delete后要手工提交事务
+        //sqlSession.commit();
+
         //8.输出结果
-        //studentList.forEach( stu -> System.out.println(stu));
-        for(Student stu : studentList){
-            System.out.println("查询的学生="+stu);
-        }
+        System.out.println("执行insert的结果="+nums);
+
         //9.关闭SqlSession对象
         sqlSession.close();
     }

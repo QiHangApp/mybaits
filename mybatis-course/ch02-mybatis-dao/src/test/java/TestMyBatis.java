88a0e8aa7b96c021df439e299e039273cffe0ebb
L QiHang <lqihang2@gmail.com>
2021-02-10 17:14:20 +0800

    mybatis如何知道调用的哪个方法

--- mybatis-course/ch02-mybatis-dao/src/test/java/TestMyBatis.java
+++ mybatis-course/ch02-mybatis-dao/src/test/java/TestMyBatis.java
@@ -13,6 +13,22 @@ public class TestMyBatis {
     @Test
     public void testSelectStudent() {
         StudentDao studentDao = new StudentDaoimpl();
+        /**
+         * List<Student> studentList = dao.selectStudents();调用
+         * 1.dao对象，类型是StudentDao，全限定名称是：com.jpowernode.dao.StudentDao
+         *      全限定名称和mapper文件中namespace是一样的。
+         *
+         * 2.方法名称，selectStudent，这个方法就是mapper文件中的id值selectStudents
+         *
+         * 3.通过dao中方法的返回值也可以确定MyBatis要调用的SqlSession的方法
+         *      如果返回值是List，调用的是SQlSession.selectList()方法。
+         *      如果返回值int，或是非List的，看mapper文件中的的标签是<insert>,<update>就会调用
+         *      SqlSession的insert，update等方法
+         *
+         * mybatis的动态代理：mybatis根据dao的方法调用，获取执行sql语句的信息。
+         *      mybatis根据你的dao接口，创建出一个dao接口的实现类，并创建这个类的对象。
+         *      完成SqlSession调用方法，访问数据库。
+         */
         List<Student> studentList = studentDao.selectStudents();
         for (Student student : studentList) {
             System.out.println(student);

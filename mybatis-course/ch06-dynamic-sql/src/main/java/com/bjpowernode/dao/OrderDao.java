76e2a6f433d1c7c2a2fb42a96a8a3f67246c5208
QiHang <553446603@qq.com>
2021-02-23 17:20:21 +0800

    数据库属性配置文件

---
+++ mybatis-course/ch06-dynamic-sql/src/main/java/com/bjpowernode/dao/OrderDao.java
@@ -0,0 +1,25 @@
+package com.bjpowernode.dao;
+
+import com.bjpowernode.domain.Student;
+
+import java.util.List;
+
+/**
+ * @author lqh
+ * @date 2021/2/9 下午9:55
+ */
+public interface OrderDao {
+
+    //动态sql,使用java对象作为参数
+    List<Student> selectStudentIf(Student student);
+
+    //where使用
+    List<Student> selectStudentWhere(Student student);
+
+    //foreach 用法 1
+    List <Student> selectForeachOne(List<Integer> idlsit);
+
+    //foreach 用法 2
+    List<Student> selectForeachTwo(List<Student> stulist);
+}
+

733fdd58dccfbe318f2c770c0a90cf53a7d915cf
QiHang <553446603@qq.com>
2021-02-23 13:20:01 +0800

    动态sql <if> <where>

--- mybatis-course/ch06-dynamic-sql/src/main/java/com/bjpowernode/dao/StudentDao.java
+++ mybatis-course/ch06-dynamic-sql/src/main/java/com/bjpowernode/dao/StudentDao.java
@@ -1,11 +1,19 @@
 package com.bjpowernode.dao;
 
+import com.bjpowernode.domain.Student;
+
+import java.util.List;
+
 /**
  * @author lqh
  * @date 2021/2/9 下午9:55
  */
 public interface StudentDao {
 
+    //动态sql,使用java对象作为参数
+    List<Student> selectStudentIf(Student student);
 
+    //where使用
+    List<Student> selectStudentWhere(Student student);
 }
 
--- mybatis-course/ch06-dynamic-sql/src/test/java/TestMyBatis.java
+++ mybatis-course/ch06-dynamic-sql/src/test/java/TestMyBatis.java
@@ -1,7 +1,10 @@
 import com.bjpowernode.dao.StudentDao;
+import com.bjpowernode.domain.Student;
 import com.bjpowernode.utils.MyBatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * @author lqh
@@ -9,13 +12,33 @@ import org.junit.Test;
  */
 public class TestMyBatis {
     @Test
-    public void testSelectStudent() {
+    public void testSelectStudentIf() {
         SqlSession sqlSession = MyBatisUtils.getSqlSession();
         //mybatis动态代理机制
         StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
 
+        Student student = new Student();
+        student.setName("李四");
+        student.setAge(18);
+        List<Student> students = studentDao.selectStudentIf(student);
+        for(Student stu: students) {
+            System.out.println("if === " + stu);
+        }
     }
+    @Test
+    public void testSelectStudentWhere() {
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        //mybatis动态代理机制
+        StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
 
+        Student student = new Student();
+        student.setName("李四");
+        student.setAge(18);
+        List<Student> students = studentDao.selectStudentWhere(student);
+        for(Student stu: students) {
+            System.out.println("where === " + stu);
+        }
+    }
 
 
 }
